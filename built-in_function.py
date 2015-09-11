0078b3312d3bccb554a53c1f27b66a02cfc765da
jue010811 <350116197@qq.com>
2015-09-11 17:49:51 +0800

    Create built-in_function.py

---
+++ built-in_function.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#函数   function
+
+#abs:求绝对值
+abs(-20)
+#20
+abs(20)
+#20
+abs(12.34)
+#12.34
+
+#cmp(x, y)    如果x<y，返回-1， 如果 x==y，返回0，如果 x>ｙ，返回１
+cmp(1, 2)
+# -1
+cmp(2, 1)
+# 1
+cmp(3, 3)
+# 0
+
+#数据类型转换
+int()
+float()
+str()
+unicode()
+bool()
